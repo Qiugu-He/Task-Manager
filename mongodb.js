1e175c97916a597865001e4fc33abb0e5438d763
Qiugu He <kaochi545121151@gmail.com>
2019-07-08 13:27:53 -0500

    added insert/search feature from mongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,12 @@
 //CRUD: create, read, update and delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient 
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017' 
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=>{
     if(error){
@@ -14,51 +16,56 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=>{
     //create&connect a db:
     const db = client.db(databaseName)  
 
-    // //select a collection and manipulate:
-    // db.collection('users').insertOne({
-    //     name:'Qiugu He',
-    //     age: 105
-    // }, (error, result)=>{
-    //     if(error){
-    //         return console.log('Unable to insert')
-    //     }
+//    //insert
+//     db.collection('tasks').insertMany([
+//         {
+//             description:'Clean house',
+//             completed:true
+//         },{
+//             description:'Renew wall',
+//             completed:false
+//         },{
+//             description:'Pot plant',
+//             completed:true
+//         }
+//     ], (error, result)=>{
+//         if(error){
+//             return console.log('Unable to insert documents!')
+//         }
 
-    //     console.log(result.ops) 
-    // })
+//         console.log(result.ops)
+//     })
 
-    // //insert multiple
-    // db.collection('users').insertMany([
-    //     {
-    //         name:'bb',
-    //         age:5
-    //     },{
-    //         name:'aa',
-    //         age:3
-    //     }
-    // ], (error, result)=>{
-    //     if(error){
-    //         return console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description:'Clean house',
-            completed:true
-        },{
-            description:'Renew wall',
-            completed:false
-        },{
-            description:'Pot plant',
-            completed:true
-        }
-    ], (error, result)=>{
+    //read
+    db.collection('users').findOne({name: 'aa'}, (error, result)=>{
         if(error){
-            return console.log('Unable to insert documents!')
+            return console.log('Unable to fetch')
         }
 
-        console.log(result.ops)
+        console.log(result)
     })
+
+    //search by id:
+    db.collection('users').findOne( {_id: new ObjectID("5d23832952cff439e4584566") }, (error, result)=>{
+        if(error){
+            return console.log('Unable to fetch')
+        }
+
+        console.log(result)
+    })
+
+    db.collection('tasks').findOne( {_id: new ObjectID("5d23800996763748e883d7d0") }, (error, result)=>{
+        if(error){
+            return console.log('Unable to fetch')
+        }
+
+        console.log(result)
+    })
+
+
+    //multiple search
+    db.collection('tasks').find( {completed: true}).toArray((error, tasks)=>{
+        console.log(tasks)
+    })
+
 })
